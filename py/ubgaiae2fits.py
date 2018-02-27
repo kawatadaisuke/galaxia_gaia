30221d9c88284e373f50905f3e5b9f345741fc62
Daisuke Kawata <d.kawata@ucl.ac.uk>
2018-02-27 18:07:57 +0000

    Output Galactic coordinates, too.

--- py/ubgaiae2fits.py
+++ py/ubgaiae2fits.py
@@ -9,6 +9,7 @@ import matplotlib.cm as cm
 from scipy import constants as const
 from FortranFile import FortranFile
 import struct
+from galpy.util import bovy_coords
 
 # input parameters
 # for distance vs. error
@@ -48,6 +49,37 @@ rdata[:,6]=180.0*rdata[:,6]/np.pi
 rdata[:,7]=180.0*rdata[:,7]/np.pi
 rdata[:,12]=180.0*rdata[:,12]/np.pi
 rdata[:,13]=180.0*rdata[:,13]/np.pi
+
+# Galactic coordinates
+# True l and b
+RA_true=rdata[:,0]
+DEC_true=rdata[:,1]
+Tllbb=bovy_coords.radec_to_lb(RA_true,DEC_true,degree=True,epoch=2000.0)
+GLON_true=Tllbb[:,0]
+GLAT_true=Tllbb[:,1]
+# True pmGLON, pmGLAT
+pmRA_true=rdata[:,3]
+pmDEC_true=rdata[:,4]
+Tpmllbb=bovy_coords.pmrapmdec_to_pmllpmbb(pmRA_true,pmDEC_true \
+   ,RA_true,DEC_true,degree=True,epoch=2000.0)
+pmGLON_true=Tpmllbb[:,0]
+pmGLAT_true=Tpmllbb[:,1]
+# observed 
+RA_obs=rdata[:,6]
+DEC_obs=rdata[:,7]
+pmRA_obs=rdata[:,9]
+pmDEC_obs=rdata[:,10]
+Tpmllbb=bovy_coords.pmrapmdec_to_pmllpmbb(pmRA_obs,pmDEC_obs \
+   ,RA_true,DEC_true,degree=True,epoch=2000.0)
+pmGLON_obs=Tpmllbb[:,0]
+pmGLAT_obs=Tpmllbb[:,1]
+# error
+e_pmRA=rdata[:,12]
+e_pmDEC=rdata[:,13]
+Tpmllbb=bovy_coords.pmrapmdec_to_pmllpmbb(e_pmRA,e_pmDEC \
+   ,RA_true,DEC_true,degree=True,epoch=2000.0)
+e_pmGLON=Tpmllbb[:,0]
+e_pmGLAT=Tpmllbb[:,1]
 
 tbhdu = pyfits.BinTableHDU.from_columns([\
   pyfits.Column(name='RA_true',unit='(degree)',format='D',array=rdata[:,0]),\
@@ -100,6 +132,24 @@ tbhdu = pyfits.BinTableHDU.from_columns([\
 # V,VI,GRVS
   pyfits.Column(name='V',unit='(mag)',format='D',array=rdata[:,42]),\
   pyfits.Column(name='V-I',unit='(mag)',format='D',array=rdata[:,43]),\
-  pyfits.Column(name='G_RVS',unit='(mag)',format='D',array=rdata[:,44])])
+  pyfits.Column(name='G_RVS',unit='(mag)',format='D',array=rdata[:,44]),\
+# GLON and GLAT only true
+  pyfits.Column(name='GLON_true',unit='(degree)',format='D',array=GLON_true),\
+  pyfits.Column(name='GLAT_true',unit='(degree)',format='D',array=GLAT_true),\
+# pmGLON and pmGLAT true
+  pyfits.Column(name='pmGLON_true',unit='(mas/yr)',format='D',\
+                array=pmGLON_true),\
+  pyfits.Column(name='pmGLAT_true',unit='(mas/yr)',format='D',\
+                array=pmGLAT_true),\
+# observed
+  pyfits.Column(name='pmGLON_obs',unit='(mas/yr)',format='D',\
+                array=pmGLON_obs),\
+  pyfits.Column(name='pmGLAT_obs',unit='(mas/yr)',format='D',\
+                array=pmGLAT_obs),\
+# Errors
+  pyfits.Column(name='e_pmGLON',unit='(mas/yr)',format='D',\
+    array=e_pmGLON),\
+  pyfits.Column(name='e_pmGLAT',unit='(mas/yr)',format='D',\
+    array=e_pmGLAT)])
 tbhdu.writeto(outfile,clobber=True)
 
